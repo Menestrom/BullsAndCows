1fb9e8a3ec42e544ed61447a8cf4a8b979fbb6f6
Menestrom <42383096+Menestrom@users.noreply.github.com>
2018-08-17 14:48:47 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,61 +1,94 @@
-﻿using System;
+using System;
+using System.Linq;
 
-namespace ConsoleApplication2
+
+namespace BullsAndCows
 {
-    class BullsandCows
+    class BullsAndCows
     {
-        int value;
-        string random;
+        static int numberLength;
+        static string finalRandom;
+        static string CheckNumber;
+        static int Bulls;
+        static int Cows;
         static void Main()
         {
-            random = Random();
-            value = Input();
+            BullsAndCows Start = new BullsAndCows();
+            numberLength = Start.Input();
+            Start.Randomizer();
+            Console.WriteLine("FinalRandom = {0}", finalRandom);
+            Start.Looped_call();
         }
-
         public int Input()
         {
-            Console.WriteLine("Insert value lenght");
-            int finalValue;
-            string input = Console.ReadLine();
-            bool value = Int32.TryParse(input, out finalValue);
-            if (value)
+            Console.WriteLine("Enter length to generate the number: ");
+            bool check = int.TryParse(Console.ReadLine(), out numberLength);
+            if (check)
             {
-                return finalValue;
+                return numberLength;
             }
             else
             {
-                Console.WriteLine("Wrong value - {0}, please intup number", input);
-                return finalValue;
+                Console.WriteLine("Wrong type! Use integer to enter length to generate the number:");
+                return numberLength;
             }
         }
-        public string Random()
-            {
-            string RandomString = "";
+        public string Randomizer()
+        {
+            
             Random rnd = new Random();
-            for (int i = 0; i < value; i++)
-            {            
-                RandomString += rnd.Next(0, 9);
-            }
-            Console.WriteLine(RandomString);
-            return RandomString;
-            }
-        public void Bulls()
-        {
-
-        }
-        public void Cows()
-        {
-
-        }
-        public void PlayerTry()
-        {
-            string n = "";
-            while (n != random)
+            for (int i = 0; i < numberLength; i++)
             {
-                Console.WriteLine("Try u'r luck");
-                n = Console.ReadLine();
+                finalRandom += rnd.Next(0,9);
             }
+            return finalRandom;
         }
 
+        public void BullsNumber()
+        {
+            char[] random_array = finalRandom.ToCharArray();
+            char[] input_number = CheckNumber.ToCharArray();
+            for (int i = 0; i < (random_array.Length); i++)
+            {
+                if (random_array[i] == input_number[i])
+                {
+                    Bulls += 1;        
+                }
+            }
+        }
+        public void CowsNumber()
+        {
+            var finalRandom_string = finalRandom.Select(c => c.ToString()).ToList();
+            var CheckNumber_string = CheckNumber.Select(c => c.ToString()).ToList();
+            int Cows_number = finalRandom_string.Intersect(CheckNumber_string).Count();
+            Cows += Cows_number;
+            if (Bulls >= Cows)
+            {
+                Cows -= Cows_number;
+            }
+        }
+        public void Looped_call()
+        {
+            while (finalRandom != CheckNumber)
+            {
+                Console.WriteLine("Please enter number with {0} - symbols length", numberLength);
+                CheckNumber = Console.ReadLine();
+                BullsNumber();
+                CowsNumber();
+                if (CheckNumber.Length > numberLength)
+                {
+                    Console.WriteLine("Wrong length!!!U entered {0}", CheckNumber.Length);
+                    
+                }
+                else if (finalRandom != CheckNumber)
+                {
+                    Console.WriteLine("Cows = {0}, Bulls = {1}", Cows, Bulls);
+                }
+                else
+                {
+                    Console.WriteLine("Congratulate, u guess the number! It's {0}", CheckNumber);
+                }
+            }
+        }
     }
 }
